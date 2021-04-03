6da18c8bccf78d7d90b57739d0185697af8a3d5f
Harsh Deep <hrshdeep51@gmail.com>
2021-04-04 04:08:51 +0530

    Add files via upload

---
+++ 125staff.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time 
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://cs125.cs.illinois.edu/")
+
+buttons = driver.find_elements_by_class_name("MuiButtonBase-root")
+buttons[1].click()
+time.sleep(2)
+
+menu_items = driver.find_elements_by_class_name("MuiListItem-root")
+menu_items[5].click()
+
+time.sleep(5) # let things load
+name_tags = driver.find_elements_by_class_name("MuiTypography-h4")
+names = [n.text for n in name_tags][5:]
+print(names)
+
+role_tags = driver.find_elements_by_class_name("MuiTypography-body2")
+roles = [r.text for r in role_tags]
+for i in range(len(names)):
+    print(names[i], roles[i])
+
+
+breakpoint()
